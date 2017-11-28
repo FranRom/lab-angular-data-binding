9055916ec1f6dcd614d5c71043456b0ad94b8ce0
FranRom <maildefranz@gmail.com>
2017-11-28 17:16:12 +0100

    Iteration 3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,25 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foodsList: Object[];
+  showPane: boolean = false;
+  newFood: Object = {};
+
 
   constructor() { }
 
   ngOnInit() {
+    this.foodsList = foods;
+    console.log(this.foodsList);
   }
 
+  showNewFoodPane() {
+    this.showPane = !this.showPane;
+  }
+
+  createNewFood() {
+    foods.push(this.newFood);
+    this.newFood = {};
+    this.showNewFoodPane();
+  }
 }
